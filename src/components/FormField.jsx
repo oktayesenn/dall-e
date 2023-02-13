05c00e604a0643cb303ae9b3373507bac3b241f9
Oktay esen <oktayesenn@gmail.com>
2023-02-13 16:10:17 -0500

    Formfield has been installed

--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 
-function FormField() {
+const FormField = ({LabelName, type, placeholder, value, handleChange, isSupriseMe, handleSupriseMe}) => {
+
   return (
-    <div>FormField</div>
+    <div>
+      <div className='flex items-center gap-2 mb-2'>
+        <label
+          htmlFor={LabelName}
+          className='block text-sm font-medium text-gray-700'
+        >
+          {LabelName}
+        </label>
+        {isSupriseMe && (
+          <button
+            type='button'
+            onClick={handleSupriseMe}
+            className= "font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black"
+          >
+            Surprise me
+          </button>
+        )}
+      </div>
+      <input 
+        type={type}
+        id={LabelName}
+        name={LabelName}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:boder-[#4649ff] outline-none block w-full p-3"
+        />
+    </div>
+
   )
 }
 

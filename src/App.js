18d649b7ee06911910193f0a3c8141070cea1cd3
Oktay esen <oktayesenn@gmail.com>
2023-02-13 12:15:56 -0500

    npm install react-router-dom

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>My First React App</h1>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
     </div>
   );
 }

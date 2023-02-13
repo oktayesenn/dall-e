41161b8a2e2fcb62cd1fe25d9e48209cf44d7390
Oktay esen <oktayesenn@gmail.com>
2023-02-13 12:26:40 -0500

    Pages created and imported

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import {Logo } from './components/Logo';
+import {Home, CreatePost} from './pages';
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </div>
+    <BrowserRouter>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+        </header>
+    </BrowserRouter>
+
   );
 }
 
